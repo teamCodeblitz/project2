48f121c541b0805ff438ce91c52648bb4d950602
Michael <202211836@gordoncollege.edu.ph>
2024-11-06 16:49:25 +0800

    Initial Commit

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,10 +3,18 @@
 
 	let email = 'user@example.com';
 	let password = 'password';
+	let isLoading = false; // New state variable to control loader visibility
 
 	function handleLogin() {
 		console.log('Logging in with', email, password);
-		window.location.href = '/project2/dashboard'; 
+		
+		isLoading = true; // Set loading state to true
+
+		// Redirect after 5 seconds
+		setTimeout(() => {
+			isLoading = false; // Reset loading state
+			window.location.href = '/project2/dashboard'; 
+		}, 3000);
 	}
 
 	function handleSubmit() {
@@ -154,8 +162,23 @@
 }
 </style>
 
+{#if isLoading} <!-- Conditional rendering for loader -->
+<div class="absolute inset-0 flex items-center justify-center bg-white bg-opacity-75 z-50">
+    
+<div role="status">
+    <svg fill="currentColor" viewBox="0 0 1792 1792" class="text-green-500 w-20 h-20 animate-spin"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+        d="M1760 896q0 176-68.5 336t-184 275.5-275.5 184-336 68.5-336-68.5-275.5-184-184-275.5-68.5-336q0-213 97-398.5t265-305.5 374-151v228q-221 45-366.5 221t-145.5 406q0 130 51 248.5t136.5 204 204 136.5 248.5 51 248.5-51 204-136.5 136.5-204 51-248.5q0-230-145.5-406t-366.5-221v-228q206 31 374 151t265 305.5 97 398.5z" />
+</svg>
+    <span class="sr-only">Loading...</span>
+</div>
+
+</div>
+{/if}
 <div class="flex flex-col min-h-screen md:flex-row">
     <div class="flex-1 bg-green-700 flex items-center justify-center relative">
+       
         <ul class="circles">
             <li></li>
             <li></li>
@@ -192,7 +215,7 @@
                 <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
                 <input type="password" id="password" bind:value={password} required class="mt-1 block w-full p-2 border border-gray-300 rounded" />
             </div>
-            <button type="submit" class="w-full bg-green-500 text-white p-2 rounded hover:bg-green-600" on:click={handleLogin}>Log In</button>
+            <button type="submit" class="w-full bg-green-500 text-white p-2 rounded hover:bg-green-600">Log In</button>
             <p class="mt-4 text-center">Don't Have an Account? 
                 <button type="button" class="text-red-500" on:click={() => window.location.href='/project2/register'}>Register</button>
             </p>
